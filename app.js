d969d334432572e32d9e6269a51781d3ddbc0512
Your Name <you@example.com>
2023-05-29 10:12:40 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,411 @@
+// ///////////// chap 1/////////////////
+// alert("muhammad");
+// alert("haseeb");
+// alert("phone");
+// alert("email");
+// alert("password");
+// ///////////////
+// alert("you are learning javascript");
+// ////////////////////////////
+// alert("#imran_khan");
+
+
+
+// ///////////////////// chap2////////////////
+// var a;
+// /////////////////
+// var a=3;
+// ///////////////
+// var teamName ="team haseeb";
+// alert(teamName);
+// //////////////////
+// var b = "charlie";
+// var b ="haseeb";
+
+
+
+// ///////////////////// chap3////////////////
+//var caseQty = 144;
+// var num = "9"
+// var rslt = 2+3;
+// console.log(rslt)
+// var merchTotal = 100;
+// var shipingCharge = 10;
+// var orderTotal = merchTotal+shipingCharge;
+// console.log(orderTotal)
+// var num = 2;
+// num = 3+5;
+// console.log(num)
+
+
+
+// ///////////////////// chap4////////////////
+// var product_cost =3.45;
+// var nameOfband =;
+// var abc = 3;
+// var firstName = "anas"
+// var lastName = "ajaz"
+// var fullNname = firstName+ " "+ lastName;
+// console.log(fullNname)
+// var abc =legal;
+// var $abc =legal
+// var 123 = illegal;
+
+
+
+// ///////////////////// chap5////////////////
+// var rslt = 5%2;num
+// console.log(rslt)
+// var num = 20%6
+// console.log(num)
+// var largeNum = 1000*2000;
+// console.log(largeNum)
+// var a = 3
+// var b = 5 - a
+// console.log(b)
+
+// ch 5 ka 5 rhta hai
+
+// var num1 = prompt("enter 1st num")
+// var num2 = prompt("enter 2nd num")
+// var rslt = prompt(num1*num2)
+
+
+
+// ///////////////chap6/////////////////
+// var x=0;
+// x++;
+// alert(x);
+// /////////////
+// var x=100;
+// x--;
+// alert(x);
+// ///////////////////////
+// var x=50 ;
+// var y=x++;
+// alert(y);
+// //////////////////
+// var y=50 ;
+// var z=--y;
+// alert(z);
+// ///////////////////
+// var q=9;
+// q--;
+// var NewNum = q ;
+// alert(NewNum);
+// //////////////
+// var q=9;
+// q++;
+// var NewNum = q ;
+// alert(NewNum);
+
+
+
+/////////////chap7///////////////
+// var calculatedNum = 2+(2*6) //ans is 14
+// alert(calculatedNum);
+// //////////////////
+// var calculatedNum = (2+2)*6
+// alert(calculatedNum); //ans is 24
+// ////////////////////////
+// var calculatedNum = (2+2)*(4+2)
+// alert(calculatedNum); //ans is 24
+// /////////////////////
+// var calculatedNum = ((2+2)*4) + 2;
+// alert(calculatedNum); //ans is 18
+///////////////////
+// var a= 2+2*4+10;
+// alert ("56");
+/////////////////////
+// var a= 2+2*4+10;
+// alert (a);
+
+
+
+// // // ////////////chap 8//////////////
+// var num = "2" + "2";
+// alert(num); // ans is 22
+// ///////////////////
+// var message = ("hello," + "dolly");
+// alert(message) // ans is hello,dolly
+// //////////////////
+// alert("33" + 3); //ans is 333
+// /////////////
+// var a = "Pakistan";
+// var b ="zindabad";
+// alert(a + b);
+// ///////////////////
+// var a = 5;
+// var b = 5;
+// var c = a + b;
+// alert(c);
+
+
+/////////////// chap 9 /////
+// var name = prompt("enter your name");
+//////////////
+// var country = prompt("country?","china")
+//////////////
+//var yourName =("enter ypur name");
+//////////////////////
+// var a = "hello sam";
+// var b = "chaelie";
+// var c =prompt(a,b);
+////////////
+// var a = "hello sam";
+// var b = "chaelie";
+// var c =prompt(a,b);
+//alert(c);
+
+
+////////////chap 10//////////////
+// var city = "karachi";
+// if(city=="karachi"){
+// console.log("city of light")
+// }
+// var x = 10;
+// var y = 10;
+// if(x===y){
+//     var z = prompt("value of z")
+//     console.log(z)
+// }
+// var code = prompt('enter code')
+// if(code==10010){
+// alert("karachi")
+// }
+// else{
+// alert("please write correct city")
+// }
+// var x =12;
+// if(x==12){
+//     x = 1
+//     console.log(x)
+// }
+
+
+/////////////// chap 11 /////
+// var a = 2;
+// var b = 3;
+// if(a==b){
+//     alert("equal")
+// }
+// else{
+//     alert("unequal")
+
+// }
+// var a = 2;
+// var b = 3;
+// if(a>=b){
+//     alert("a is greater")
+// }
+// else{
+//     alert("b is greater")
+
+// }
+// var a = 3;
+// if (a != 2) {
+//     a = 4;
+//     console.log(a)
+// }
+// var a = 2;
+// var b = 3;
+// if(a!=b){
+// alert("congrates")
+// }
+// var firstname =prompt("name")
+// var lastName= "ajaz"
+// if(firstname!=lastName){
+// alert("no match")
+// }
+
+
+/////////////// chap 12 /////
+
+// var a = 2;
+// var b = 3
+// if (a >= b) {
+//     alert("a is greater or equal")
+// }
+// else {
+//     alert("a is not greater or equal")
+// }
+// var mark = prompt("enter mark")
+// var percentage = (mark / 100) * 100
+// if (percentage >= 70 && percentage <= 79) {
+//     alert("grade A")
+// }
+// else if (percentage >= 80 && percentage <= 100) {
+//     alert("grade A++")
+// }
+// else {
+//     alert("fail")
+// }
+// var a = 7
+// if (a === 10) {
+//     alert("correct")
+// } else {
+//     alert("correct value of a:" + " " + a)
+// }
+// var city = prompt("enter city")
+// if (city == "karachi") {
+//     alert("yes it is karachi")
+// } else if (city == "lahore") {
+//     alert("yes it is lahore")
+// }
+// else {
+//     alert(" it is other city")
+// }
+
+
+
+/////////////// chap 13 /////
+// var a=3
+// var b=3
+// var c=6
+// var d=6
+// if ( a==b && c==d){
+//     alert ("a is eqal to b and c is equal to d")
+
+// }else {
+//     alert("your statement is false")
+// }
+///////////////
+// var a=3
+// var b=3
+// var c=6
+// var d=5
+// if ( a==b && c!=d){
+//     alert ("a is eqal to b and c is not equal to d")
+// }else {
+//     alert("your statement is false")
+// }
+////////////////
+// var name1 = "hamza"
+// var name2 = "arslan"
+// var age = 60
+// if (name1 == "hamza" || name2 == "arslan" && age == 60) {
+//     alert("you are eligible")
+// }
+// else {
+//     alert("not eligible")
+// }
+////////////////////
+// var a =5
+// var b=10
+// if(a<b || a>b){
+//     alert("a is not equal to b")
+// }
+/////////////
+// var fname= prompt("enter your firstname")
+// var lname= prompt("enter your lastname")
+// if(fname== lname){
+//     alert("your first and last names should not equal")
+// }
+
+
+/////////////// chap 14 /////
+// var pswd = prompt("enter passward")
+// if (pswd != "") {
+//     if (pswd < 5) {
+//         alert("passward must be greater than 5")
+//     }
+//     else {
+//         alert("ok")
+//     }
+// }
+// else {
+//     alert("please enter passward")
+// }
+// var a = 1;
+// var c = "max";
+// if (a === 1) {
+//     if (c === "min") {
+//         alert("ok")
+//     }
+//     else if (c === "max") {
+//         alert("ok")
+//     }
+
+// }
+// var a = 2;
+// var b = 2;
+// if (a = b) {
+//     if (a <= b) {
+//         document.write("pass")
+//     }
+// }
+// else {
+//     alert("test pass")
+// }
+
+
+// ///////////////////////chap 15////////////////////
+// var arr = []
+// console.log(arr);
+
+// //////////////////////////
+// var arr = ["Array"];
+// console.log(arr);
+// ///////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alert(2);
+// ////////////////
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// console.log(alphabet.length);
+//////////////////////////////
+// var arr = ["aa", "bb"]
+// alert(arr[0] + arr[1])
+
+
+
+// ///////////////////////////chap16/////////////////
+// var string = ["Javascript"]
+// string.push("HTML");
+// console.log(string);
+// ///////////////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alphabet.pop();
+// ///////////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alphabet.push("l");
+// ////////////////////////
+// var alphabet = ["h", "i", "j", "k"];
+// alphabet.push("l");
+// /////////////////////////////
+// var Sizes = ["Sm", "Lg", "Xl", "XXL"];
+// Sizes.shift(0, 1);
+// onsole.log(Sizes);c
+// //////////////////////////
+// var Sizes = ["Sm", "Lg", "Xl", "XXL"];
+// Sizes.unshift(1, 2, 3);
+// console.log(Sizes);
+///////////////////////////
+
+///slice splice ko dkhna hai///
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "cow"]
+// var newpets = pets.slice(0, 2);
+// console.log(newpets)
+
+
+// ///////////////////////////chap 17 to 20/////////////////
+// for (i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+// for (i = 0; i <= 12; i++) {
+//     console.log(i)
+// }
+// for(var i=0; i<=4; i++){
+
+// }
+// for(j=0; j<=100; j++){
+
+// }
+// for (i = 3; i > 3; i++) {
+
+//  }
+
+
+
+
+
